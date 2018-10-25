1c84a8f50575d1547ced9a5938277a8533d8ddf3
vadimsevelinski <shevelinski@gmail.com>
2018-10-26 00:26:52 +0300

    corrected home-task8

--- lesson-8-oop/script.js
+++ lesson-8-oop/script.js
@@ -1,23 +1,36 @@
-function Question(_quaere, _answer) {
-    this.quaere = _quaere;
-    this.answer = _answer;
-    this.show = function () {
-        console.log(this.quaere + " ? " + '\n'
-            + this.answer);
-    }
+function Question(_quaere, _answer, _correctAnswer) {
+    this.question = _quaere;
+    this.answers = _answer;
+    this.correctAnswer = _correctAnswer;
 }
-ans1 = ['1.суббота', '\n', '2.пятница', '\n', '3.не помню'];
-ans2 = ['1.да', '\n', '2.нет', '\n', '3.кто вы такие, я вас не звал'];
-var question1 = new Question('Какой сегодня день', ans1.join(' '));
-var question2 = new Question('class позволяет задавать свойства-значения?', ans2.join(' '));
-answer_result = function () {
-    var ans3 = prompt('введите правильный ответ');
-    if (ans3 === '2') {
-        console.log('cool!')
-    } else {
-        console.log('иди учись')
-    }
 
+var question1 = new Question('Какой сегодня день?', ['суббота', 'пятница', 'не помню'], 2);
+var question2 = new Question('class позволяет задавать свойства-значения?', ['да', 'нет', 'кто вы такие, я вас не звал'], 1);
+var question3 = new Question('кто проживает на дне окана?', ['дубка гоб', 'спанч боб', 'кто вы такие, я вас не звал'], 2);
+var questions = [question1, question2, question3];
+
+function selectOneRandom(questionsArray) {
+    return questionsArray[(Math.ceil(Math.random() * (questionsArray.length)) - 1)];
+}
+
+Question.prototype.printQuestion  = function () {
+    var strAnswers = '';
+    for (var i = 1; i <= this.answers.length; i++) {
+        strAnswers = strAnswers + i + ')' + this.answers[i - 1] + ';' + '\n';
+    }
+    console.log(this.question + '\n' + strAnswers);
 };
-question1.show();
-answer_result();
+
+var examQuestion = selectOneRandom(questions);
+examQuestion.printQuestion();
+var userAnswer = parseInt(prompt('Номер вашего варианта ответа'), 10);
+
+Question.prototype.checkAnswer = function (answer) {
+    if (answer === this.correctAnswer) {
+        console.log('и это правильный ответ!');
+    } else {
+        console.log('вы самое слабое звено. прощайте \n Правильный ответ: ' + this.correctAnswer);
+    }
+};
+
+examQuestion.checkAnswer(userAnswer);
